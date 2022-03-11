a7156056836b56b414f287bc6d17149e3796eaa2
Hendry Lim <myhendry@gmail.com>
2022-03-11 09:56:26 +0800

    added delete

--- BLOCKCHAIN-1/contracts/Demo.sol
+++ BLOCKCHAIN-1/contracts/Demo.sol
@@ -1,16 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
+
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Demo {
     string public name = "Yess";
     // dynamic array
     uint256[] public nums = [1, 2, 3];
-
-    // uint[3] p
+    // fixed array
+    uint256[3] public nums_fixed = [1, 2, 3];
+    // mapping
+    mapping(address => uint256) public balances;
 
     constructor() {
         name = "Hello Demo";
+        nums.push(9);
+    }
+
+    // Console.log
+    function transfer(string memory _name) public view returns (string memory) {
+        console.log("name", _name);
+        return _name;
+    }
+
+    // Update Value in Array
+    function change(uint256 _num) external {
+        nums[1] = _num;
+    }
+
+    // Get Array
+    function getArr() external view returns (uint256[] memory) {
+        return nums;
+    }
+
+    // Method 1: Remove Element: Remove with Last Value and Pop
+    function remove(uint256 _index) external {
+        nums[_index] = nums[nums.length - 1];
+        nums.pop();
+    }
+
+    // Remove Value in Array
+    // Value at _index will be set to 0
+    function removeValue(uint256 _index) external {
+        delete nums[_index];
     }
 }
--- BLOCKCHAIN-1/test/demo_test.ts
+++ BLOCKCHAIN-1/test/demo_test.ts
@@ -1,16 +1,20 @@
-import { Demo } from "./../typechain-types/Demo";
 import { Deployment } from "hardhat-deploy/types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import { deployments, ethers } from "hardhat";
 import { expect } from "chai";
 
+import { Demo } from "./../typechain-types/Demo";
+
 describe("Demo Test", () => {
   let DemoContract: Deployment;
   let demoContract: Demo;
+  let deployer: SignerWithAddress;
+  let user2: SignerWithAddress;
+  let user3: SignerWithAddress;
 
   before(async () => {
-    // const accounts: SignerWithAddress[] = await ethers.getSigners();
-    //     const [deployer, user2, user3] = accounts;
+    const accounts: SignerWithAddress[] = await ethers.getSigners();
+    [deployer, user2, user3] = accounts;
 
     await deployments.fixture(["demo"]);
 
@@ -30,6 +34,41 @@ describe("Demo Test", () => {
     it("should returns nums[0]", async () => {
       const num = await demoContract.nums(0);
       expect(num.toString()).to.be.equal("1");
+    });
+
+    it("should console", async () => {
+      const res = await demoContract.connect(user2).transfer("yep");
+      expect(res).to.be.equal("yep");
+    });
+
+    it("should change nums[1]", async () => {
+      let beforeChangeNum;
+      let afterChangeNum;
+      beforeChangeNum = await demoContract.nums(1);
+      expect(beforeChangeNum.toString()).to.be.equal("2");
+
+      await demoContract.connect(user2).change(1000);
+
+      afterChangeNum = await demoContract.nums(1);
+      expect(afterChangeNum).to.be.equal("1000");
+    });
+
+    it("should remove item in nums", async () => {
+      const numsArr = await demoContract.getArr();
+      expect(numsArr.length).to.be.equal(4);
+      demoContract.remove(1);
+      const numsArrAfter = await demoContract.getArr();
+      expect(numsArrAfter.length).to.be.equal(3);
+    });
+
+    it("should delete value in nums", async () => {
+      const numsArr = await demoContract.getArr();
+      expect(numsArr.length).to.be.equal(3);
+      //console.log(numsArr);
+      demoContract.removeValue(1);
+      const numsArrAfter = await demoContract.getArr();
+      //console.log(numsArrAfter);
+      expect(numsArrAfter.length).to.be.equal(3);
     });
   });
 });
